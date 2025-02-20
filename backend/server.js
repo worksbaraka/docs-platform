d79537447e62823a25d1bef5b3efeae4f5423734
Works <worksbarakah@gmail.com>
2025-02-21 00:51:36 +0300

    Dockerfile init

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// In-memory storage for the document (for demonstration purposes)
+let currentDocument = '';
+
 // Test route
 app.get('/', (req, res) => {
   res.send('Backend is running!');
 });
 
-// Create an HTTP server and attach Socket.IO
+// Endpoint to retrieve the current document
+app.get('/document', (req, res) => {
+    res.json({ document: currentDocument });
+  });
+  
+  // Endpoint to update the document
+  app.post('/document', (req, res) => {
+    const { document } = req.body;
+    currentDocument = document;
+    res.json({ message: 'Document updated', document: currentDocument });
+  });
+
+// Create an HTTP server, attach Socket.IO
 const PORT = process.env.PORT || 5000;
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
@@ -20,7 +35,7 @@ const io = require('socket.io')(http);
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // Listen for document updates and broadcast to others
+  // Listen for doc updates and broadcast
   socket.on('doc-update', (data) => {
     socket.broadcast.emit('doc-update', data);
   });
@@ -30,5 +45,5 @@ io.on('connection', (socket) => {
   });
 });
 
-// Start the server
+// Start server
 http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
